23276ce0a9aee48aff98e35228c02f209b144049
Boese0601 <dichang@usc.edu>
2025-05-23 14:15:17 -0700

    add bytemorph web

---
+++ bytemorph/js/globals.js
@@ -0,0 +1,8 @@
+// globals.js
+window.GLOBALS = {
+    dataset: "ByteMorph-6M",
+    framework: "ByteMorph",
+    model: "ByteMorpher",
+    benchmark: "ByteMorph-Bench",
+    paperTitle: "ByteMorph: Benchmarking Instruction-Guided Image Editing with Non-Rigid Motions",
+};
